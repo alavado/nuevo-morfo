ccdf6e94d51653da41d73502e4582a34a1700f52
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-01-17 22:37:37 -0300

    agrega mapa stamen

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.css'
+import Mapa from '../Mapa'
 
 const App = () => {
   return (
     <div className="App">
-      app
+      <Mapa />
     </div>
   )
 }
--- src/components/Mapa/Mapa.js
+++ src/components/Mapa/Mapa.js
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReactMapGL from 'react-map-gl'
 
 const Mapa = () => {
+
+  const [viewport, setViewport] = useState({
+    width: '100%',
+    height: '100vh',
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8,
+  })
+
+  const mapStyle = {
+    version: 8,
+    sources: {
+      'raster-tiles': {
+        type: 'raster',
+        tiles: [
+          'https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg'
+        ],
+        tileSize: 256,
+        attribution: 'Map tiles by <a target="_top" rel="noopener" href="http://stamen.com">Stamen Design</a>, under <a target="_top" rel="noopener" href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a target="_top" rel="noopener" href="http://openstreetmap.org">OpenStreetMap</a>, under <a target="_top" rel="noopener" href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>'
+      }
+    },
+    layers: [
+      {
+        id: 'simple-tiles',
+        type: 'raster',
+        source: 'raster-tiles',
+        minzoom: 0,
+        maxzoom: 22
+      }
+    ]
+  }
+
   return (
-    <div>
-      mapa
-    </div>
+    <ReactMapGL
+      {...viewport}
+      onViewportChange={setViewport}
+      mapStyle={mapStyle}
+    />
   )
 }
 
