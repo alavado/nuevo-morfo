460c982ccc8ba570afa7908de80d33fa568229e6
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-01-17 23:22:51 -0300

    respaldo comando dzsave

---
+++ server/tiles.js
@@ -0,0 +1,6 @@
+// este es el comando:
+// .\server\tools\vips-dev-8.9\bin\vips.exe
+//    dzsave 
+//    .\server\images\parque_croacia\parque_croacia.jpg
+//    .\server\images\parque_croacia\pyramid
+//    --layout google
