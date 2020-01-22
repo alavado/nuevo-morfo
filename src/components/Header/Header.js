85ff7e81f283673a0853a364b3e7da2bac230e59
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-01-22 10:56:35 -0300

    corrige seccion seleccionada

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,10 +3,14 @@ import './Header.css'
 import query from '../../queries/secciones'
 import { useQuery } from '@apollo/react-hooks'
 import { isDev } from '../../helpers/dev'
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { fijarSeccion } from '../../redux/actions'
 
 const Header = () => {
 
+  const dispatch = useDispatch()
+  const seccionSeleccionada = useSelector(state => state.seccion.seccion)
   const { loading, error, data } = useQuery(query)
 
   const autenticarConUCampus = () => {
@@ -19,15 +23,20 @@ const Header = () => {
     <header>
       <div id="brand">Morfo</div>
       <nav>
-        {!loading && data.secciones.map(({ id, nombre }, i) => (
-          <NavLink
-            key={id}
-            to={`/seccion/${id}`}
-            activeClassName="seccion-activa"
+        {!loading && data.secciones.map((seccion, i) => (
+          <Link
+            key={seccion.id}
+            to={`/seccion/${seccion.id}`}
+            className={
+              seccionSeleccionada &&
+              seccionSeleccionada.id === seccion.id &&
+              'seccion-activa'
+            }
+            onClick={e => dispatch(fijarSeccion(seccion))}
             style={{ animationDelay: `${.15 * (data.secciones.length - i)}s` }}
           >
-            {nombre}
-          </NavLink>
+            {seccion.nombre}
+          </Link>
         ))}
         <a href="#" onClick={autenticarConUCampus}>Acceder</a>
       </nav>
