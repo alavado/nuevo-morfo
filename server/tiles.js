8a1f36ae3cf3b781d8476c3c82a6a242d2aa2148
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-01-30 11:11:47 -0300

    respalda comando para thumbnails

--- server/tiles.js
+++ server/tiles.js
@@ -6,3 +6,7 @@
 //    --layout google
 
 //mkdir .\server\images\IMG_0688 && mv .\server\images\IMG_0688.JPG .\server\images\IMG_0688\IMG_0688.JPG && mkdir .\server\images\IMG_0688\pyramid && .\server\tools\vips-dev-8.9\bin\vips.exe dzsave .\server\images\IMG_0688\IMG_0688.JPG .\server\images\IMG_0688\pyramid --layout google
+
+
+// para hacer thumbnails
+//./server/tools/vips-dev-8.9/bin/vipsthumbnail.exe ./server/images/5e2afdf0d5860c5154afb22d/original.jpg --smartcrop centre -s 128
