be919d2163b8faa51771284f43776733d99d7a9e
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-02-03 13:13:41 -0300

    agrega componente para buscadorg

---
+++ src/components/Buscador/Buscador.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useRef } from 'react'
+import './Buscador.css'
+import { useSelector } from 'react-redux'
+
+const Buscador = () => {
+
+  const { seccion } = useSelector(state => state.navegacion)
+  const contenedor = useRef()
+
+  useEffect(() => {
+    if (seccion) {
+      contenedor.current.classList.add('compacto')
+    }
+    else {
+      contenedor.current.classList.remove('compacto')
+    }
+  }, [seccion])
+
+  return (
+    <div ref={contenedor} className="contenedor-buscador">
+      xx
+    </div>
+  )
+}
+
+export default Buscador
--- src/components/Principal/Principal.js
+++ src/components/Principal/Principal.js
@@ -2,15 +2,16 @@ import React from 'react'
 import './Principal.css'
 import Mapa from '../Mapa'
 import { Switch, Route } from 'react-router-dom'
+import Buscador from '../Buscador'
 
 const Principal = () => {
   return (
     <section className="contenedor-principal">
       <Switch>
         <Route path="/contenido/:id" component={Mapa} />
-        <Route path="/subseccion/:id" component={() => "selecciona un contenido"} />
-        <Route path="/seccion/:id" component={() => "selecciona un contenido"} />
-        <Route path="/" component={() => "bienvenido"} />
+        <Route path="/subseccion/:id" component={Buscador} />
+        <Route path="/seccion/:id" component={Buscador} />
+        <Route path="/" component={Buscador} />
       </Switch>
     </section>
   )
