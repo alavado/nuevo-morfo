1d226af0d1ffd09eda1f81924dcf84d66ddf315a
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-02-09 11:40:46 -0300

    agrega mensaje para cuando no hay subseccione

--- src/components/Lateral/MenuSubseccion/MenuSubseccion.js
+++ src/components/Lateral/MenuSubseccion/MenuSubseccion.js
@@ -6,6 +6,7 @@ import { useDispatch } from 'react-redux'
 import { fijarSeccion, fijarSubseccion, fijarContenido, mostrarNavegacion } from '../../../redux/actions'
 import './MenuSubseccion.css'
 import Loader from '../../Loader'
+import _ from 'lodash'
 
 const MenuSubseccion = () => {
 
@@ -23,32 +24,30 @@ const MenuSubseccion = () => {
     dispatch(mostrarNavegacion())
   }, [])
 
-  const ListaContenidos = () => loading ? null :
-    <ul className="lista-items">
-      {data.subseccion.contenidos
-        .sort((s1, s2) => s1.titulo > s2.titulo ? 1 : -1)
-        .map((contenido, i) => (
-          <li
-            key={contenido.id}
-            style={{
-              animationDelay: `${i * .05}s`,
-              textDecoration: contenido.deleted ? 'line-through' : 'none'
-            }}
-          >
-            <Link
-              to={`/contenido/${contenido.id}`}
-              onClick={e => dispatch(fijarContenido(contenido))}
+  const ListaContenidos = () => loading ? <Loader /> :
+    (_.isEmpty(data.subseccion.contenidos) ?
+      <p className="mensaje-lista-vacia">No hay contenidos</p> :
+      <ul className="lista-items">
+        {data.subseccion.contenidos
+          .sort((s1, s2) => s1.titulo > s2.titulo ? 1 : -1)
+          .map((contenido, i) => (
+            <li
               key={contenido.id}
+              style={{
+                animationDelay: `${i * .05}s`,
+                textDecoration: contenido.deleted ? 'line-through' : 'none'
+              }}
             >
-              {contenido.titulo}
-            </Link>
-          </li>
-      ))}
-    </ul>
-
-  if (loading) {
-    return <Loader />
-  }
+              <Link
+                to={`/contenido/${contenido.id}`}
+                onClick={e => dispatch(fijarContenido(contenido))}
+                key={contenido.id}
+              >
+                {contenido.titulo}
+              </Link>
+            </li>
+        ))}
+      </ul>)
   
   return (
     <div className="contenedor-lista">
