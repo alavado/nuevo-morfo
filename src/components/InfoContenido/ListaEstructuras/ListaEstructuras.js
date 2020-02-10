0e6e36af52d579661a6c83af3a202eccffec871d
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-02-10 12:37:44 -0300

    agrega instrucciones para agregar marcadores

--- src/components/InfoContenido/ListaEstructuras/ListaEstructuras.js
+++ src/components/InfoContenido/ListaEstructuras/ListaEstructuras.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { destacarMarcador, dejarDeDestacarMarcador, mostrarPopup, esconderPopup, fijarDestino } from '../../../redux/actions'
 import './ListaEstructuras.css'
+import _ from 'lodash'
 
 const ListaEstructuras = () => {
 
@@ -11,28 +12,32 @@ const ListaEstructuras = () => {
   return (
     <div className="lista-estructuras">
       <h4>Estructuras</h4>
-      <ul>
-        {contenido.imagenes[indiceImagenActual].marcadores.map((marcador, i) => (
-          <li
-            key={marcador.id}
-            style={{ animationDelay: `${i * .15}s` }}
-            onMouseEnter={() => {
-              dispatch(destacarMarcador(marcador))
-              dispatch(mostrarPopup({
-                ...marcador,
-                titulo: `${marcador.titulo}-${marcador.id}`
-              }))
-            }}
-            onMouseLeave={() => {
-              dispatch(dejarDeDestacarMarcador())
-              dispatch(esconderPopup())
-            }}
-            onClick={() => dispatch(fijarDestino({ lat: marcador.lat, lng: marcador.lng }))}
-          >
-            <a className="nombre-estructura">{marcador.titulo} - {marcador.id}</a>
-          </li>
-        ))} 
-      </ul>
+      {_.isEmpty(contenido.imagenes[indiceImagenActual].marcadores) ?
+        <><p className="mensaje-lista-vacia">No hay estructuras en esta imagen.</p>
+        <p className="mensaje-lista-vacia">Recuerda que puedes agregar estructuras haciendo click derecho sobre la imagen</p></> : 
+        <ul>
+            {contenido.imagenes[indiceImagenActual].marcadores.map((marcador, i) => (
+              <li
+                key={marcador.id}
+                style={{ animationDelay: `${i * .15}s` }}
+                onMouseEnter={() => {
+                  dispatch(destacarMarcador(marcador))
+                  dispatch(mostrarPopup({
+                    ...marcador,
+                    titulo: `${marcador.titulo}-${marcador.id}`
+                  }))
+                }}
+                onMouseLeave={() => {
+                  dispatch(dejarDeDestacarMarcador())
+                  dispatch(esconderPopup())
+                }}
+                onClick={() => dispatch(fijarDestino({ lat: marcador.lat, lng: marcador.lng }))}
+              >
+                <a className="nombre-estructura">{marcador.titulo} - {marcador.id}</a>
+              </li>
+            ))}
+        </ul>
+      } 
     </div>
   )
 }
