0707e4a8ff46b6fb2f1fbe22256a49fa8824881b
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-02-11 15:47:22 -0300

    corrige comparacion para tildes

--- src/helpers/utiles.js
+++ src/helpers/utiles.js
@@ -1,3 +1,3 @@
 export const compararPropiedadString = prop => {
-  return (obj1, obj2) => obj1[prop].toLocaleUpperCase() > obj2[prop].toLocaleUpperCase() ? 1 : -1
+  return (obj1, obj2) => obj1[prop].toLocaleUpperCase().localeCompare(obj2[prop].toLocaleUpperCase())
 }
