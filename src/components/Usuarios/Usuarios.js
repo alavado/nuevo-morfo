fde166aa43ab1aeb9685b68a7272547607609095
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-02-14 20:01:47 -0300

    agrega boton de limpieza de busqueda

--- src/components/Usuarios/Usuarios.js
+++ src/components/Usuarios/Usuarios.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import query from '../../queries/usuarios'
 import './Usuarios.css'
 import { useQuery } from '@apollo/react-hooks'
 import MiLoader from '../Loader'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch as iconoBuscar } from '@fortawesome/free-solid-svg-icons'
+import { faTimes as iconoLimpiarBusqueda } from '@fortawesome/free-solid-svg-icons'
+import _ from 'lodash'
 
 const Usuarios = () => {
 
+  const [terminoBusqueda, setTerminoBusqueda] = useState('')
   const { loading, error, data } = useQuery(query)
 
   if (loading) {
@@ -14,16 +19,39 @@ const Usuarios = () => {
 
   return (
     <div className="contenedor-tabla-grande">
+      <div className="encabezado-tabla">
+        <h1>Usuarios</h1>
+        <div className="busqueda">
+          <FontAwesomeIcon
+            className="icono-buscar"
+            icon={iconoBuscar}
+            size="sm"
+          />
+          <input
+            type="text"
+            value={terminoBusqueda}
+            onChange={e => setTerminoBusqueda(e.target.value)}
+          />
+          {!_.isEmpty(terminoBusqueda) && <FontAwesomeIcon
+            className="icono-limpiar-busqueda"
+            icon={iconoLimpiarBusqueda}
+            size="sm"
+            onClick={e => setTerminoBusqueda('')}
+          />}
+        </div>
+      </div>
       <table>
         <thead>
           <th>Nombre</th>
           <th>E-mail</th>
+          <th>Grupos</th>
         </thead>
         <tbody>
           {data.usuarios.map(u => (
             <tr>
               <td>{u.nombre}</td>
               <td>{u.email}</td>
+              <td></td>
             </tr>
           ))}
         </tbody>
