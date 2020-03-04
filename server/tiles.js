11d9e0bf2a3c06f8d832b760572ff9baf00819c4
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-03-04 16:46:42 -0300

    agrega vips para centos

--- server/tiles.js
+++ server/tiles.js
@@ -1,29 +1,17 @@
-// este es el comando:
-// .\server\tools\vips-dev-8.9\bin\vips.exe
-//    dzsave 
-//    .\server\images\parque_croacia\parque_croacia.jpg
-//    .\server\images\parque_croacia\pyramid
-//    --layout google
-
-//mkdir .\server\images\IMG_0688 && mv .\server\images\IMG_0688.JPG .\server\images\IMG_0688\IMG_0688.JPG && mkdir .\server\images\IMG_0688\pyramid && .\server\tools\vips-dev-8.9\bin\vips.exe dzsave .\server\images\IMG_0688\IMG_0688.JPG .\server\images\IMG_0688\pyramid --layout google
-
-
-// para hacer thumbnails
-//./server/tools/vips-dev-8.9/bin/vipsthumbnail.exe ./server/images/5e2afdf0d5860c5154afb22d/original.jpg --smartcrop centre -s 128
-
 const childProcess = require('child_process')
 const path = require('path')
+const isDev = require('./helpers/dev').isDev()
 
 const crearThumbnail = archivo => {
-  const pathVipsThumbnail = path.resolve('.\\server\\tools\\vips-dev-8.9\\bin\\vipsthumbnail.exe')
   const original = path.resolve(`.\\server\\images\\${archivo}\\original.jpg`)
+  const pathVipsThumbnail = isDev ? path.resolve('.\\server\\tools\\vips-dev-8.9\\bin\\vipsthumbnail.exe') : 'vipsthumbnail'
   childProcess.execSync(`${pathVipsThumbnail} ${original} --smartcrop centre -s 128`)
 }
 
 const crearPiramide = archivo => {
-  const pathVips = path.resolve(`.\\server\\tools\\vips-dev-8.9\\bin\\vips.exe`)
   const destino = path.resolve(`.\\server\\images\\${archivo}\\pyramid`)
   const original = path.resolve(`.\\server\\images\\${archivo}\\original.jpg`)
+  const pathVips = isDev ? path.resolve(`.\\server\\tools\\vips-dev-8.9\\bin\\vips.exe`) : 'vips'
   childProcess.execSync(`mkdir ${destino} && ${pathVips} dzsave ${original} ${destino} --layout google`)
 }
 
@@ -31,3 +19,20 @@ module.exports = {
   crearThumbnail,
   crearPiramide
 }
+
+/*
+Instalé vips desde github según sale en:
+
+https://libvips.github.io/libvips/install.html
+
+pero con esta corrección:
+
+I was able to resolve the issue by following these steps.
+Added usrlocal.conf file with following line into /etc/ld.so.conf.d
+
+/usr/local/lib
+
+Then ran ldconfig -v as root upon save
+
+Thank you very much for the help.
+*/
