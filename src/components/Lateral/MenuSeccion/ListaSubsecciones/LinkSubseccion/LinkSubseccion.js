8ce88c5f88b5b24c830cde8f8123a1032e0f9e95
Alejandro Lavado <alejandro.lvd@gmail.com>
2021-08-27 12:13:33 -0400

    refactor link subseccion

---
+++ src/components/Lateral/MenuSeccion/ListaSubsecciones/LinkSubseccion/LinkSubseccion.js
@@ -0,0 +1,68 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { esAdmin } from '../../../../../helpers/auth'
+import { fijarSubseccion } from '../../../../../redux/actions'
+import { faCaretRight, faEdit as iconoEditar } from '@fortawesome/free-solid-svg-icons'
+import { faTrashAlt as iconoEliminar } from '@fortawesome/free-solid-svg-icons'
+import eliminarSubseccionMutation from '../../../../../mutations/eliminarSubseccion'
+import query from '../../../../../queries/seccion'
+import { Link, useParams } from 'react-router-dom'
+import _ from 'lodash'
+import './LinkSubseccion.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { useMutation } from '@apollo/react-hooks'
+
+const LinkSubseccion = ({ subseccion, i }) => {
+
+  const { usuario } = useSelector(state => state.auth)
+  const [eliminarMutate] = useMutation(eliminarSubseccionMutation)
+  const dispatch = useDispatch()
+  const { id } = useParams()
+
+  const eliminar = idSubseccion => {
+    eliminarMutate({
+      variables: { id: idSubseccion },
+      refetchQueries: [{ query, variables: { id } }]
+    })
+  }
+
+  return (
+    <li
+      className="LinkSubseccion"
+      key={subseccion.id}
+      style={{ animationDelay: `${i * .05}s` }}
+    >
+      <Link
+        to={`/subseccion/${subseccion.id}`} key={subseccion.id}
+        onClick={e => dispatch(fijarSubseccion(subseccion))}
+      >
+        <FontAwesomeIcon
+          className="ListaSubsecciones__icono_link_subseccion"
+          icon={faCaretRight}
+        />
+        {subseccion.nombre}
+      </Link>
+      {usuario && esAdmin(usuario) && <div className="acciones-subseccion">
+        {_.isEmpty(subseccion.contenidos.filter(c => !c.deleted)) &&
+          <>
+            <button
+              className="boton-eliminar-subseccion"
+              onClick={() => eliminar(subseccion.id)}
+              title={`Editar nombre "${subseccion.nombre}"`}
+            >
+              <FontAwesomeIcon icon={iconoEditar} />
+            </button>
+            <button
+              className="boton-eliminar-subseccion"
+              onClick={() => eliminar(subseccion.id)}
+              title={`Eliminar subsección "${subseccion.nombre}"`}
+            >
+              <FontAwesomeIcon icon={iconoEliminar} />
+            </button>
+          </>
+        }
+      </div>}
+    </li>
+  )
+}
+
+export default LinkSubseccion
--- src/components/Lateral/MenuSeccion/ListaSubsecciones/ListaSubsecciones.js
+++ src/components/Lateral/MenuSeccion/ListaSubsecciones/ListaSubsecciones.js
@@ -1,64 +1,22 @@
 import React from 'react'
-import { useMutation } from '@apollo/react-hooks'
-import { useDispatch, useSelector } from 'react-redux'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrashAlt as iconoEliminar } from '@fortawesome/free-solid-svg-icons'
-import { Link, useParams } from 'react-router-dom'
-import eliminarSubseccionMutation from '../../../../mutations/eliminarSubseccion'
 import { compararPropiedadString } from '../../../../helpers/utiles'
-import { fijarSubseccion } from '../../../../redux/actions'
-import query from '../../../../queries/seccion'
 import _ from 'lodash'
-import { faCaretRight } from '@fortawesome/free-solid-svg-icons'
-import { esAdmin } from '../../../../helpers/auth'
 import './ListaSubsecciones.css'
+import LinkSubseccion from './LinkSubseccion'
 
 const ListaSubsecciones = ({ data }) => {
-
-  const [eliminarMutate] = useMutation(eliminarSubseccionMutation)
-  const { usuario } = useSelector(state => state.auth)
-  const dispatch = useDispatch()
-  const { id } = useParams()
-
-  const eliminar = idSubseccion => {
-    eliminarMutate({
-      variables: { id: idSubseccion },
-      refetchQueries: [{ query, variables: { id } }]
-    })
-  }
 
   return (
     <ul className="lista-items">
       {data.seccion.subsecciones
         .sort(compararPropiedadString('nombre'))
         .map((subseccion, i) => (
-          <li
+          <LinkSubseccion
+            i={i}
+            subseccion={subseccion}
             key={subseccion.id}
-            style={{ animationDelay: `${i * .05}s` }}
-          >
-            <Link
-              to={`/subseccion/${subseccion.id}`} key={subseccion.id}
-              onClick={e => dispatch(fijarSubseccion(subseccion))}
-            >
-              <FontAwesomeIcon
-                className="ListaSubsecciones__icono_link_subseccion"
-                icon={faCaretRight}
-              />
-              {subseccion.nombre}
-            </Link>
-            {usuario && esAdmin(usuario) && <div className="acciones-subseccion">
-              {_.isEmpty(subseccion.contenidos.filter(c => !c.deleted)) &&
-                <button
-                  className="boton-eliminar-subseccion"
-                  onClick={() => eliminar(subseccion.id)}
-                  title={`Eliminar subsección "${subseccion.nombre}"`}
-                >
-                  <FontAwesomeIcon icon={iconoEliminar} />
-                </button>
-              }
-            </div>}
-          </li>
-      ))}
+          />
+        ))}
     </ul>
   )
 }
